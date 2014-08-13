04c59917485cf1a078dc6417feecb87236e52f00
Ben Read <endymion1818@gmail.com>
2014-08-13 17:29:17 +0100

    start of onourway page, H6 = Trebuchet

---
+++ onourway.php
@@ -0,0 +1,40 @@
+<?php
+  $page_title = 'Home &#124; Citylink - Get More';
+  $meta_desc = "#";
+include 'header.php'; ?>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <section class="row">
+      <div class="container">
+        <div class="col-sm-8">
+        <blockquote>
+          <h3>“On Our Way is a brilliant offering giving our customers the opportunity to take control of their deliveries.”</h3>
+          <attr>Charlie Lucas, Display Fix.</attr>
+        </blockquote>
+        <h1>Improving your customerss delivery experience</h1>
+        <p>On Our Way, our estimated time of arrival service, keeps your customers informed and gives them greater control over their deliveries. And what’s more the standard service is completely free*. Our helpful interactive solution lets your customers know what time their delivery will arrive – and easily reschedule or rearrange, if they wish!</p>
+        <p>Once a delivery has been organised, we’ll give your customer a two-hour delivery time slot so they can plan their day efficiently, without having to wait around for goods to arrive. If the delivery time doesn’t suit them, they simply reschedule for another day or arrange for the parcel to be delivered to a neighbour, left in a safe place (if you give them this option), or collected from their local City Link depot. And B2B customers will benefit too, they will know in advance when to expect us allowing them to schedule activities dependant on the arrival.</p>
+        <h6>Everyone benefits</h6>
+        <ul>
+          <li>Your customers will know when to expect us making it easier to plan their day</li>
+          <li>Your customers will receive goods at a time convenient to them thanks to our
+        rescheduling and alternative delivery options</li>
+          <li>You’ll save time, hassle and money due to fewer delivery enquiries</li>
+          <li>We’ll increase your customer satisfaction levels by improving first time deliveries and overall customer experience</li>
+        <h6>How does it work?</h6>
+        <p>Once your parcel is scanned into our network we will send your customer a notification by email or SMS text* to let them know we will deliver the next day. On the morning of delivery we will send them another notification advising them of the estimated two-hour delivery time slot. The notifications will also give your customers the option to reschedule or track their parcel online.</p>
+        <p>If you want to find out more, including details on the different messaging options, download our leaflet. The PDF should be on the right navigation and as an inline link.</p>
+        <p class="small">*Standard email notifications are free of charge. SMS text messages will incur a small additional charge.</p>
+        <br/>
+        <h6>How does it work?</h6>
+        <p>Getting started is really easy. Simply, click on the button that reflects the booking/labelling solution you use to find out everything you need to know.</p>
+      </div>
+      <div class="col-md-4">
+      <?php include 'sidebar-testimonial.php';?>
+      <?php include 'sidebar-read.php';?>
+      <?php include 'sidebar-findout.php';?>
+      </div>
+    </div>
+  </section>
+
+<?php include 'footer.php';?>
